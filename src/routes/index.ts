50d22e5bd8d80f5e1e12ee6fe56b2ac0d1f413aa
A1n2F <alissonfferreira22@gmail.com>
2025-05-07 10:28:43 -0300

    ensure-authenticated

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,10 +4,14 @@ import { usersRoutes } from "./users-routes";
 import { sessionsRoutes } from "./sessions-routes";
 import { refundsRoutes } from "./refunds-routes";
 
+import { ensureAuthenticated } from "../middlewares/ensure-authenticated";
+
 const routes = Router()
 
 routes.use("/users", usersRoutes)
 routes.use("/sessions", sessionsRoutes)
+
+routes.use(ensureAuthenticated)
 routes.use("/refunds", refundsRoutes)
 
 export { routes }
